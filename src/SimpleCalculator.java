01bd3123895d1e2e124c01117f4fc2ba86d08c16
aidansmccormick <34948272+aidansmccormick@users.noreply.github.com>
2018-03-22 20:01:54 -0700

    Goud day

--- src/SimpleCalculator.java
+++ src/SimpleCalculator.java
@@ -1,25 +1,48 @@
 
-
 import javax.swing.JOptionPane;
 
 public class SimpleCalculator {
 	public static void main(String[] args) {
 
 		// 1. Get 2 numbers from the user and convert them to integer.
+		String but = JOptionPane.showInputDialog(" ");
+		String bat = JOptionPane.showInputDialog(" ");
+		int in1 = Integer.parseInt(but);
+		int in2 = Integer.parseInt(bat);
 
 		// 2. Customize pop-up to support add/subtract/multiply/divide operations.
-		int operation = JOptionPane.showOptionDialog(null, "The question", "Pop-up Title", 0,
-				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Button1", "Button2", "Button3", "Button3" },
-				null);
+		int operation = JOptionPane.showOptionDialog(null, "add/subtract/multiply/divide", "Pop-up Title", 0,
+				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "add", "subtract", "multiply", "divide" }, null);
+		if (operation == 1) {
 
-		// 5. Call the methods created in steps 3 and 4 to perform the appropriate operation.
+			int asnA = in1 - in2;
+			JOptionPane.showMessageDialog(null, asnA);
+		}
+		if (operation == 0) {
+
+			int asnB = in1 + in2;
+			JOptionPane.showMessageDialog(null, asnB);
+		}
+		if (operation == 2) {
+
+			int asnC = in1 * in2;
+			JOptionPane.showMessageDialog(null, asnC);
+		}
+		if (operation == 3) {
+
+			int asnD = in1 / in2;
+			JOptionPane.showMessageDialog(null, asnD);
+		}
+		// 5. Call the methods created in steps 3 and 4 to perform the appropriate
+		// operation.
 
 		// 6. Wrap steps 1, 2 and 5 in a for loop to repeat calculator operations
 	}
 
 	// 3. Create method for addition operation.
-	//    Method should create pop-up that shows equation and solution, eg. 45 + 25 = 70.
-	//    HINT: use 'static void add(int num1, num2) { ... }
+	// Method should create pop-up that shows equation and solution, eg. 45 + 25 =
+	// 70.
+	// HINT: use 'static void add(int num1, num2) { ... }
 
 	// 4. Create similar methods for subtraction, multiplication and division.
 }
--- src/SleepyHead.java
+++ src/SleepyHead.java
@@ -1,28 +1,44 @@
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
 import javax.swing.JOptionPane;
 
-
 public class SleepyHead {
 
+	public static void main(String[] args) {
 
-    public static void main(String[] args) {
-        
-        boolean isWeekday, isVacation;
-        
-        /*
-         * Ask the user for these values using
-         * JOptionPane.showConfirmDialog(null, "is it a weekday?", "",
-         * JOptionPane.YES_NO_OPTION);
-         */
-    
+		// Ask the user for these values using
+		int day = JOptionPane.showConfirmDialog(null, "is it a weekday?");
 
+		if (day == 1) {
 
-        /*
-         * Print “sleep in” if it is a vacation or a weekend. If it’s a weekday,
-         * print “get up lazybones!” If it is a weekday, and we are on vacation,
-         * print “sleep in”.
-         */
-    }
+			JOptionPane.showMessageDialog(null, "sleep in.");
+
+		}
+
+		if (day == 0) {
+
+			int vac = JOptionPane.showConfirmDialog(null, "are you on vacation?");
+
+			if (vac == 1) {
+
+				JOptionPane.showMessageDialog(null, "wake up lazybones.");
+
+			}
+
+			if (vac == 0) {
+
+				JOptionPane.showMessageDialog(null, "sleep in.");
+
+			}
+
+		}
+
+		/*
+		 * Print “sleep in” if it is a vacation or a weekend. If it’s a weekday, print
+		 * “get up lazybones!” If it is a weekday, and we are on vacation, print “sleep
+		 * in”.
+		 */
+	}
 }
